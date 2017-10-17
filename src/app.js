9dc65dbb107db904dd65024701013fc1f0101c61
kenlyau <newallah@gmail.com>
2017-10-17 22:13:30 +0800

    router

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import {HashRouter, Switch, Route} from 'react-router-dom'
+import PrivateRoute from './components/private-route'
+import Login from './containers/login'
+import Home from './containers/home'
 
 export default class App extends React.Component {
   render () {
-    return <h1>app start</h1>
+    return (
+      <HashRouter>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <PrivateRoute path="/" component={Home} />
+        </Switch>
+      </HashRouter>
+    )
   }
 }
