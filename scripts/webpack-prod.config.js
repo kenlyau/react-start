5dc04b95c0e85778fb03ce9fe4e43c4f4b906b2a
kenlyau <newallah@gmail.com>
2017-10-27 17:01:00 +0800

    opt

--- scripts/webpack-prod.config.js
+++ scripts/webpack-prod.config.js
@@ -1,6 +1,31 @@
+const WebPack = require('webpack')
 const webpackMerage = require('webpack-merge')
 const webpackBaseConfig = require('./webpack-base.config')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-const settings = {}
+const settings = {
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+    new WebPack.optimize.UglifyJsPlugin(),
+    new WebPack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new BundleAnalyzerPlugin()
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: 'css-loader'
+        })
+      }
+    ]
+  }
+}
 
 module.exports = webpackMerage(webpackBaseConfig, settings)
